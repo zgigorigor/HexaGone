0ee421e04a24544688f4685d0261771dee43dfc9
Igor, PC <bistrovic.igor@gmail.com>
2020-09-04 15:18:12 +0200

    game over

---
+++ HexaGone/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public static bool GamePause = false;
+    public GameObject gameOverUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
